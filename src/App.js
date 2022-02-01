abcd503a0a3e6adf57da16eb1f41189ba9e12ac3
mg24rodriguez <mg24rodriguez@gmail.com>
2022-01-31 22:01:10 -0500

    lista con jsx
    primer ejercicio de create react app con jsx

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
-import logo from './logo.svg';
+import logo from './logo.png';
+import React from 'react';
 import './App.css';
 
 function App() {
@@ -6,18 +7,21 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1> Hola Dojo ya se React!</h1>
       </header>
+
+      <body className = "App-body">
+        <h2>Cosas que necesito hacer</h2>
+
+        <lu>
+            <li>Mejorar mi lenguaje JavaScrip y React</li>
+            <li>Escalar el Chimborazo</li>
+            <li>Aprendera manejar moto</li>
+            <li>Comer y no engordar</li>
+            <li>Hacer mi portafolio </li>
+        </lu>  
+
+      </body>   
     </div>
   );
 }
